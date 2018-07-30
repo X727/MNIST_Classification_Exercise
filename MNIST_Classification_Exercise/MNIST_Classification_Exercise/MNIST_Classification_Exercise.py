17f23aee97ba9b11be39038895549ea8d12c3da6
Patrick Ignoto <patrick.ignoto@gmail.com>
2018-07-30 19:30:02 -0400

    Print how long the calculations took to determine the data

--- MNIST_Classification_Exercise/MNIST_Classification_Exercise/MNIST_Classification_Exercise.py
+++ MNIST_Classification_Exercise/MNIST_Classification_Exercise/MNIST_Classification_Exercise.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split, cross_val_score, cross_val
 from sklearn.linear_model import LogisticRegression
 from sklearn import tree
 from sklearn.metrics import confusion_matrix, precision_score, recall_score
+import time
 
 def main():
     mnist = fetch_mldata('MNIST original')
@@ -26,16 +27,20 @@ def main():
     y_test_0 = (y_test == 0)
 
     for type in clfs:
-        scores = cross_val_score(clfs[type], X_train, y_train_0, cv=10, scoring='accuracy')
-        print("Cross-validation scores for {}:\n".format(type))
+        start_time = time.time()
+        scores = cross_val_score(clfs[type], X_train, y_train_0, cv=5, scoring='accuracy')
+        print("\nCross-validation scores for {}:\n".format(type))
         print(scores)
         y_pred = cross_val_predict(clfs[type], X_test, y_test_0)
         cm = confusion_matrix(y_test_0, y_pred)
-        print("Confusion matrix for {}:\n".format(type))
+        print("\nConfusion matrix for {}:\n".format(type))
         print(cm)
         rs = recall_score(y_test_0, y_pred)
         ps = precision_score(y_test_0, y_pred)
-        print("Recall is {0:.2f} \t Precision is: {0:.2f}".format(rs,ps))
+        print("Recall is {0:.2f} \tPrecision is: {0:.2f}\n".format(rs,ps))
+        stop_time = time.time()
+        delta = stop_time - start_time
+        print("Elapsed time for determining data is {} seconds\n".format(delta))
 
 if __name__ == '__main__':
     main()
